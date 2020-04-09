6376a73227c9d9509e31c0d8ccb2740f7b3d9494
Kamil Hamerlak <39203194+kmlZ@users.noreply.github.com>
2020-04-09 13:59:15 +0200

    Some updates

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#Kamil Hamerlak, April 2020
+
 from user import User
 import re
 import os.path
@@ -12,7 +14,6 @@ current_user_name = ""
 current_user_password = ""
 user_to_remove = ""
 
-
 def main():
     
     if not os.path.exists('login_system/users'):
@@ -22,7 +23,16 @@ def main():
         print("Create root account \n")
         create_root()
 
-    user_input = input("login, add, remove or edit: ").lower()
+    print("*"*15)
+    print("Commands")
+    print(":login")
+    print(":add")
+    print(":remove or rm")
+    print(":edit")
+    print(":quit")
+    print("*"*15)
+    user_input = input(": ").lower()
+    
 
     if user_input == "login":
         username_input = input("Type username: ")
@@ -32,16 +42,21 @@ def main():
         create_user()
     elif user_input == "remove" or user_input == "rm":
         remove_user()
-    elif user_input == "edit":
-        user_edit()
+    elif user_input == "edit": 
+        user_edit(input("Type username to edit: "))
+    elif user_input == "quit":
+        quit()
+    else:
+        print("Wrong command...\n")
+        main()
 
 def create_user():
-    username = input("Wpisz nazwę użytkownika: ")
-    userpass = input("Wpisz hasło: ")
-    useremail = input("Wpisz email: ")
+    username = input("Type username: ")
+    userpass = input("Type password: ")
+    useremail = input("Type email: ")
     if not validate_email(useremail):
         print("Invalid email... retype")
-        useremail = input("Wpisz email: ")
+        useremail = input("Type email: ")
 
     name = User(username,userpass,useremail)
 
@@ -74,11 +89,15 @@ def login(username, password):
                 is_logged_in = True
                 current_user_name = user_file[0].replace("\n", "") 
                 current_user_password = user_file[1].replace("\n", "")
+                main()
+            else:
+                print("Wrong username or password...\n")
+                main()
                 if user_file[3].replace("\n", "") == "root":
                     is_logged_in_as_root = True
                 else:
                     is_logged_in_as_root = False
-                main()
+
         except Exception as error:
             print("User not found in database")
             print(str(error))           
@@ -87,23 +106,32 @@ def login(username, password):
 def remove_user():
     global user_to_remove
 
-    user_to_remove = input("Wpisz nazwę użytkownika do usunięcia: ")
+    user_to_remove = input("Type accoutn name to remove: ")
     if is_logged_in_as_root == True and current_user_name != user_to_remove:
         os.remove("login_system/users/" + user_to_remove + ".txt")
         main()
     elif is_logged_in_as_root != True :
-        print("You aren't allowed to remove user. Log in as root")
+        print("You aren't allowed to remove user. Log in as root...")
         root_input = input("Type root account username: ")
-        root_input_passwd = input("Type root accoutn password: ")
+        root_input_passwd = input("Type root account password: ")
         login(root_input, root_input_passwd)
-    
-
-    
-    
-def user_edit():
-    return
-
-
+      
+def user_edit(file_name):
+    file_exists = True
+    while file_exists:
+        try:
+            with open("login_system/users/" + file_name + ".txt", "r") as f:
+                f.readlines()
+            os.system('notepad ' + "login_system/users/" + file_name + ".txt")
+            with open("login_system/users/" + file_name + ".txt", "r") as k:
+                edited_user_file = k.readlines()
+            if edited_user_file[0] != file_name:
+                os.rename("login_system/users/" + file_name + ".txt", "login_system/users/" + edited_user_file[0].replace("\n","") + ".txt")
+            main()
+        except Exception as err:
+            file_exists = False
+            print("Something went wrong in user_edit() funstion, or account doesn't exists..." + "\n" + f"error message {err}")
+            
 
 def create_root():
     name = input("Type root accoutn name: ")
@@ -128,13 +156,10 @@ def create_root():
         print("Password too short.")
         main()
 
-     
 def validate_email(email):
     if re.search(pattern, email):
         return True
     else:
         return False
-
-    
 
 main()
